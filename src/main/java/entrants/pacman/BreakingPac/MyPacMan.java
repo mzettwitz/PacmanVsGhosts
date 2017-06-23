02cb5e60b95ea0407fae5a9d029dc25d9c5a7e6f
Martin Zettwitz <martin.zettwitz@st.ovgu.de>
2017-06-23 13:30:11 +0200

    stable pacmanAI
    issue #2

--- src/main/java/entrants/pacman/BreakingPac/MyPacMan.java
+++ src/main/java/entrants/pacman/BreakingPac/MyPacMan.java
@@ -1,6 +1,8 @@
 package entrants.pacman.BreakingPac;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 import pacman.controllers.MASController;
 import pacman.controllers.PacmanController;
@@ -20,6 +22,7 @@ public class MyPacMan extends PacmanController {
 	private Mapping m_map;
 	private Policy m_policy;    
 	private MASController m_ghosts;
+	private ArrayList<Integer> m_visitedNodes; 
 
 
 	// Constructor: 100ms to compute: build up initial map
@@ -27,42 +30,76 @@ public class MyPacMan extends PacmanController {
 	{
 		m_policy = new Policy();
 		m_map = new Mapping();
-		m_ghosts = new POCommGhosts(50); 
+		m_ghosts = new POCommGhosts(50);
+		m_visitedNodes = new ArrayList<Integer>();
 	}
 
 	public MOVE getMove(Game game, long timeDue) {
 		//Place your game logic here to play the game as Ms Pac-Man
 
-		MOVE [] moves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
-		int [] rewards = new int [moves.length];
-		int max = 0;
-		int moveIdx = 0;
+		MOVE [] _moves = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
+		ArrayList<MOVE> moves = new ArrayList<MOVE>();
+		for(int i = 0; i < _moves.length; i++)
+			moves.add(_moves[i]);
+
+		//ArrayList<Integer> rewards = new ArrayList<Integer>(moves.length);
+		ArrayList<Pair<Integer, MOVE> > rewardedMoves = new ArrayList<Pair<Integer, MOVE> >(moves.size());
+		int nodeIdx = game.getPacmanCurrentNodeIndex();
 
 		// prepare simulated game
 		Game simGame;
-        GameInfo info = game.getPopulatedGameInfo();
-        info.fixGhosts((ghost) -> new Ghost(
-                ghost,
-                game.getCurrentMaze().lairNodeIndex,
-                -1,
-                -1,
-                MOVE.NEUTRAL
-        ));
-        simGame = game.getGameFromInfo(info);
-        
-        // simulate all moves
-		for (int i = 0; i < moves.length; i++)
+		GameInfo info = game.getPopulatedGameInfo();
+		info.fixGhosts((ghost) -> new Ghost(
+				ghost,
+				game.getCurrentMaze().lairNodeIndex,
+				-1,
+				-1,
+				MOVE.NEUTRAL
+				));
+		simGame = game.getGameFromInfo(info);
+
+		// simulate all moves
+		for (int i = 0; i < moves.size(); i++)
 		{
-			rewards[i] = computeMove(0, 5, moves[i], game.getPacmanCurrentNodeIndex(), simGame);
-			if(rewards[i] > max && moves[i] != lastMove.opposite())
+			rewardedMoves.add(i, new Pair<Integer, MOVE>());
+			rewardedMoves.get(i).first = computeMove(0, 5, moves.get(i), nodeIdx, simGame);
+			rewardedMoves.get(i).second = moves.get(i);
+		}
+
+		// sort moves to get best moves in ascending order
+		sortRewardedMoves(rewardedMoves);
+		// loop descending 
+		for(int i = rewardedMoves.size()-1; i >= 0; i--)
+		{
+			// check if next node was visited before
+			int neighbor = game.getNeighbour(nodeIdx, rewardedMoves.get(i).second);
+			// add next node to visited nodes
+			if(!m_visitedNodes.contains(neighbor))
 			{
-				max = rewards[i];
-				moveIdx = i;
+				m_visitedNodes.add(neighbor);
+				return rewardedMoves.get(i).second;
 			}
 		}
 
-		return moves[moveIdx];
-
+		// if all neighbor nodes were visited: do random step
+		// TODO: use heuristics
+		Random rnd = new Random();
+		int rndMove = 0;
+		if(game.isJunction(nodeIdx))
+		{		
+			rndMove = rnd.nextInt(moves.size());
+			while(game.getPacmanLastMoveMade() == moves.get(rndMove).opposite())
+				rndMove = rnd.nextInt(moves.size());
+		}		
+		else if(moves.contains(lastMove))
+			return lastMove;		
+		else
+		{
+			rndMove = rnd.nextInt(moves.size());
+			while(game.getPacmanLastMoveMade() == moves.get(rndMove).opposite())
+				rndMove = rnd.nextInt(moves.size());
+		}
+		return	moves.get(rndMove);
 	}
 
 
@@ -82,14 +119,14 @@ public class MyPacMan extends PacmanController {
 		for (int i = 0; i < moves.length; i++) {			
 			gameCopies[i] = game.copy();
 			gameCopies[i].advanceGame(moves[i],m_ghosts.getMove(gameCopies[i], 40));
-			
+
 			int idx = gameCopies[i].getPacmanCurrentNodeIndex();
 			if(idx == -1)
 				continue;
 
 			rewards[i] = game.getScore();
-			
-			
+
+
 			/*if(gameCopies[i].wasPillEaten())
 			//if(idx != -1 && game.getPillIndex(idx) != -1 && gameCopies[i].isPillStillAvailable(game.getPillIndex(idx)))	
 				rewards[i] = m_policy.pill;
@@ -140,5 +177,28 @@ public class MyPacMan extends PacmanController {
 		int powerPill = 10;
 		int powerPillGhost = 100;
 		int ppGhostMulti = -2;	// use ghost * ppMulti while PP is active
+	}
+
+
+	private class Pair<F, S> 
+	{
+		F first;
+		S second;
+	}
+
+	private void sortRewardedMoves(ArrayList<Pair<Integer, MOVE> > list)
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			for(int j = 0; j < list.size()-i-1; j++)
+			{
+				if(list.get(j).first > list.get(j+1).first)
+				{
+					Pair<Integer, MOVE> tmp = list.get(j+1);
+					list.set(j+1, list.get(j));
+					list.set(j, tmp);
+				}					
+			}
+		}
 	}
 }
