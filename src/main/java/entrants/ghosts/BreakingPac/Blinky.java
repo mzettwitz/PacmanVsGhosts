102a68fb0d19ae39d7cedb522d3f2c03f0fe1768
Andreaskusnezov <andreaskusnezov@web.de>
2017-07-02 19:36:48 +0200

    Search for some normal active Pill to protect
    If no PowerPills are active anymore, search for an active normal Pill and protect it with your life

--- src/main/java/entrants/ghosts/BreakingPac/Blinky.java
+++ src/main/java/entrants/ghosts/BreakingPac/Blinky.java
@@ -1,6 +1,7 @@
 package entrants.ghosts.BreakingPac;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import pacman.controllers.IndividualGhostController;
 import pacman.controllers.MASController;
@@ -19,26 +20,43 @@ public class Blinky extends IndividualGhostController {
         super(Constants.GHOST.BLINKY);
     }
 public static int powerPillIndex = 0;
+public static int pillIndex = -1;
+private int targetPill = -1;
+Random rnd = new Random();
     @Override
     public Constants.MOVE getMove(Game game, long timeDue) {
-    	// simply obtain all possible moves(remember: ghost are only able to run forward and left/right, but stay at position
-		Constants.MOVE lastMove = game.getGhostLastMoveMade(GHOST.BLINKY);
 		int currentPosition = game.getGhostCurrentNodeIndex(GHOST.BLINKY);
 		int currentPacPosition = game.getPacmanCurrentNodeIndex();
-		int targetPowerPill = game.getPowerPillIndices()[powerPillIndex];
-		if(currentPosition == targetPowerPill && !game.isPowerPillStillAvailable(game.getPowerPillIndex(targetPowerPill)))
-		{
-			powerPillIndex = Pinky.powerPillIndex;
+		if(Sue.pillIndex > 0 || Inky.powerPillIndex > 0 || Pinky.powerPillIndex > 0){
+			pillIndex = Math.max(Math.max(Sue.pillIndex, Pinky.pillIndex), Inky.pillIndex);
 		}
-		int distanceToPowerPill = game.getShortestPathDistance(currentPosition,targetPowerPill);
-		int distancePacToPowerPill = game.getShortestPathDistance(currentPacPosition, targetPowerPill);
-		if(distanceToPowerPill > distancePacToPowerPill){
-			Constants.MOVE move = game.getNextMoveTowardsTarget(currentPosition, targetPowerPill, Constants.DM.PATH);
-			return move;
-		}else{
-		    Constants.MOVE move = game.getApproximateNextMoveTowardsTarget(currentPosition,
-		            currentPacPosition, lastMove, Constants.DM.PATH);
-		    return move;
-		}
+    	if(game.getNumberOfPowerPills() == 0 && pillIndex == -1 && !game.isPillStillAvailable(currentPosition)){
+            Constants.MOVE[] possibleMoves = game.getPossibleMoves(currentPosition, game.getGhostLastMoveMade(GHOST.BLINKY));
+            return possibleMoves[rnd.nextInt(possibleMoves.length)];
+    	}else{
+			Constants.MOVE lastMove = game.getGhostLastMoveMade(GHOST.BLINKY);
+			if(game.getNumberOfActivePills() != 0){
+				targetPill = game.getPowerPillIndices()[powerPillIndex];
+				if(currentPosition == targetPill && !game.isPowerPillStillAvailable(game.getPowerPillIndex(targetPill)))
+				{
+					powerPillIndex = Pinky.powerPillIndex;
+				}
+			}else if(pillIndex == -1){
+				pillIndex = currentPosition;
+				
+			}else{
+				targetPill = pillIndex;
+			}
+			int distanceToPowerPill = game.getShortestPathDistance(currentPosition,targetPill);
+			int distancePacToPowerPill = game.getShortestPathDistance(currentPacPosition, targetPill);
+			if(distanceToPowerPill > distancePacToPowerPill){
+				Constants.MOVE move = game.getNextMoveTowardsTarget(currentPosition, targetPill, Constants.DM.PATH);
+				return move;
+			}else{
+			    Constants.MOVE move = game.getApproximateNextMoveTowardsTarget(currentPosition,
+			            currentPacPosition, lastMove, Constants.DM.PATH);
+			    return move;
+			}
+    	}
     }
 }
--- src/main/java/entrants/ghosts/BreakingPac/Pinky.java
+++ src/main/java/entrants/ghosts/BreakingPac/Pinky.java
@@ -6,6 +6,8 @@ import pacman.game.Constants;
 import pacman.game.Game;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by Piers on 11/11/2015.
@@ -16,26 +18,43 @@ public class Pinky extends IndividualGhostController {
         super(Constants.GHOST.PINKY);
     }
     public static int powerPillIndex = 2;
+    public static int pillIndex = -1;
+    private int targetPill = -1;
+    Random rnd = new Random();
     @Override
     public Constants.MOVE getMove(Game game, long timeDue) {
-    	Constants.MOVE lastMove = game.getGhostLastMoveMade(GHOST.PINKY);
     	int currentPosition = game.getGhostCurrentNodeIndex(GHOST.PINKY);
-    	int currentPacPosition = game.getPacmanCurrentNodeIndex();
-    	int targetPowerPill = game.getPowerPillIndices()[powerPillIndex];
-		if(currentPosition == targetPowerPill && !game.isPowerPillStillAvailable(game.getPowerPillIndex(targetPowerPill)))
-		{
-			powerPillIndex = Sue.powerPillIndex;
+		int currentPacPosition = game.getPacmanCurrentNodeIndex();
+		if(Sue.pillIndex > 0 || Inky.powerPillIndex > 0 || Blinky.powerPillIndex > 0){
+			pillIndex = Math.max(Math.max(Sue.pillIndex, Blinky.pillIndex), Inky.pillIndex);
 		}
-    	int distanceToPowerPill = game.getShortestPathDistance(currentPosition,targetPowerPill);
-    	int distancePacToPowerPill = game.getShortestPathDistance(currentPacPosition, targetPowerPill);
-    	if(distanceToPowerPill > distancePacToPowerPill){
-    		Constants.MOVE move = game.getNextMoveTowardsTarget(currentPosition, targetPowerPill, Constants.DM.PATH);
-    		return move;
+    	if(game.getNumberOfPowerPills() == 0 && pillIndex == -1 && !game.isPillStillAvailable(currentPosition)){
+            Constants.MOVE[] possibleMoves = game.getPossibleMoves(currentPosition, game.getGhostLastMoveMade(GHOST.PINKY));
+            return possibleMoves[rnd.nextInt(possibleMoves.length)];
     	}else{
-            Constants.MOVE move = game.getApproximateNextMoveTowardsTarget(currentPosition,
-                    currentPacPosition, lastMove, Constants.DM.PATH);
-            return move;
-    	} 
-    	    
+			Constants.MOVE lastMove = game.getGhostLastMoveMade(GHOST.PINKY);
+			if(game.getNumberOfActivePills() != 0){
+				targetPill = game.getPowerPillIndices()[powerPillIndex];
+				if(currentPosition == targetPill && !game.isPowerPillStillAvailable(game.getPowerPillIndex(targetPill)))
+				{
+					powerPillIndex = Sue.powerPillIndex;
+				}
+			}else if(pillIndex == -1){
+				pillIndex = currentPosition;
+				
+			}else{
+				targetPill = pillIndex;
+			}
+			int distanceToPowerPill = game.getShortestPathDistance(currentPosition,targetPill);
+			int distancePacToPowerPill = game.getShortestPathDistance(currentPacPosition, targetPill);
+			if(distanceToPowerPill > distancePacToPowerPill){
+				Constants.MOVE move = game.getNextMoveTowardsTarget(currentPosition, targetPill, Constants.DM.PATH);
+				return move;
+			}else{
+			    Constants.MOVE move = game.getApproximateNextMoveTowardsTarget(currentPosition,
+			            currentPacPosition, lastMove, Constants.DM.PATH);
+			    return move;
+			}
+    	}
     }
 }
